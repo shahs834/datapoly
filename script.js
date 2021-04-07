482757984254ecf7035384e6659ccab0d4038139
Suditi <shahs834@newschool.edu>
2021-04-07 00:16:59 -0400

    8

---
+++ script.js
@@ -0,0 +1,36 @@
+//THIS IS A COMMENT 
+
+//Beginning
+$(document).ready(function(){ 
+//this means that once all the html is loaded (ready) java will perform functions
+
+//Stuff happens here
+
+
+
+
+//
+//<style>
+//  .content {
+//  display: none;
+// }
+//</style>
+
+//<script>
+//get element by id, selet 'c1', show for .1sec, then move on to next one, then loop
+//then, after x number of sec, generte random # between 0-14
+//then get element by id, select 'c' + random(), and show that one.
+
+//</script>
+
+
+
+
+
+
+
+
+
+
+//End
+});
