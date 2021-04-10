ce42efb77bc18fd4a9620ee35791584a05e234e3
Suditi <shahs834@newschool.edu>
2021-04-10 15:56:58 -0400

    6

--- script.js
+++ script.js
@@ -1,36 +1,10 @@
-//THIS IS A COMMENT 
-
-//Beginning
-$(document).ready(function(){ 
-//this means that once all the html is loaded (ready) java will perform functions
-
-//Stuff happens here
-
-
-
-
-//
-//<style>
-//  .content {
-//  display: none;
-// }
-//</style>
-
-//<script>
-//get element by id, selet 'c1', show for .1sec, then move on to next one, then loop
-//then, after x number of sec, generte random # between 0-14
-//then get element by id, select 'c' + random(), and show that one.
-
-//</script>
-
-
-
-
-
-
-
-
-
-
-//End
+const element = document.getElementById('machine');
+const machine = new SlotMachine(element, {
+  active: 1,
+  delay: 1000,
+  spins: 5,
+  randomize() {
+    return this.random;
+  }
 });
+machine.shuffle();
